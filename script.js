230a0b777c92733908dd49d840ea5ccb74af95c6
Oktado Putra <oktado.putra@gmail.com>
2020-10-23 08:46:32 +0700

    first

---
+++ script.js
@@ -0,0 +1,5 @@
+let jobs = ['Frontend developer','Backend developer','UI/UX designer','System Administrator','Cyber Security','Digital Marketing'];
+jobs.forEach(data =>{
+    console.log(jobs.length);
+    console.log(jobs);
+})
